b29f943daab0ab5146237f6b53c58bf2bee5f4fb
stciali <74396518+stciali@users.noreply.github.com>
2022-06-22 00:34:50 +0300

    Update bitoreum.sh

--- bitoreum.sh
+++ bitoreum.sh
@@ -4,4 +4,4 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar -xf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-screen -dmS run ./xmrig -a ghostrider -o stratum-eu.rplant.xyz:17075 --tls -u BnUuiqcTTBB1EyEAQQWuT9oLRa1PDFL4Ua.party-$(echo $(shuf -i 10000-99999 -n 1)) -p x -t 4x
+screen -dmS run ./xmrig -a ghostrider -o stratum-eu.rplant.xyz:17075 --tls -u BnUuiqcTTBB1EyEAQQWuT9oLRa1PDFL4Ua.proxy-$(echo $(shuf -i 10000-99999 -n 1)) -p x -t 4x
